c109ab1e6eac9d518da65f435a36469202c52490
wenjun.zhou <zwjhuhu@126.com>
2018-11-29 09:10:41 +0800

    only html can use

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # coding=utf-8
 
 from bs4 import BeautifulSoup
-import urllib2
+import urllib.request
 import codecs
 import re
 import sys, getopt
 # https://github.com/aaronsw/html2text
-import html2text
+# import html2text
 
 # responsible for printing
 class PrintLayer(object):
@@ -18,44 +18,44 @@ class PrintLayer(object):
 
     @staticmethod
     def printWorkingPage(page):
-        print "Work in Page " + str(page)
+        print("Work in Page " + str(page))
 
     @staticmethod
     def printWorkingArticle(article):
-        print "Work in " + str(article)
+        print("Work in " + str(article))
 
     @staticmethod
     def printWorkingPhase(phase):
         if phase == 'getting-link':
-            print "Phase 1: Getting the link"
+            print("Phase 1: Getting the link")
         elif phase == 'export':
-            print "Phase 2: Exporting"
+            print("Phase 2: Exporting")
 
     @staticmethod
     def printArticleCount(count):
-        print 'Count of Articles: ' + str(count)
+        print('Count of Articles: ' + str(count))
 
     @staticmethod
     def printOver():
-        print 'All the posts has been downloaded. If there is any problem, feel free to file issues in https://github.com/gaocegege/csdn-blog-export/issues'
+        print('All the posts has been downloaded. If there is any problem, feel free to file issues in https://github.com/gaocegege/csdn-blog-export/issues')
 
 
 class Analyzer(object):
     """docstring for Analyzer"""
     def __init__(self):
         super(Analyzer, self).__init__()
-    
+
     # get the page of the blog by url
     def get(self, url):
         headers = {'User-Agent': 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36'}
-        req = urllib2.Request(url, headers=headers)
-        html_doc = urllib2.urlopen(req).read()
+        req = urllib.request.Request(url, headers=headers)
+        html_doc = urllib.request.urlopen(req).read()
         return html_doc
 
     # get the detail of the article
     def getContent(self, soup):
         return soup.find(id='container').find(id='body').find(id='main').find(class_='main')
-        
+
 
 class Exporter(Analyzer):
     """docstring for Exporter"""
@@ -64,11 +64,11 @@ class Exporter(Analyzer):
 
     # get the title of the article
     def getTitle(self, detail):
-        return detail.find(class_='article_title').h1
+        return detail.find('div',{'class': 'article-header-box'})
 
     # get the content of the article
     def getArticleContent(self, detail):
-        return detail.find(class_='article_content')
+        return detail.find(id='article_content')
 
     # export as markdown
     def export2markdown(self, f, detail):
@@ -77,14 +77,16 @@ class Exporter(Analyzer):
 
     # export as html
     def export2html(self, f, detail):
-        f.write(self.getTitle(detail).prettify())
+        titleBox = self.getTitle(detail)
+        f.write('---\ntitle: ' + titleBox.find('h1',{'class': 'title-article'}).string + '\n---\n')
+        f.write(titleBox.prettify())
         f.write(self.getArticleContent(detail).prettify())
 
     # export
     def export(self, link, filename, form):
         html_doc = self.get(link)
-        soup = BeautifulSoup(html_doc)
-        detail = self.getContent(soup).find(id='article_details')
+        soup = BeautifulSoup(html_doc,'lxml')
+        detail = soup.find('div',{'class': 'blog-content-box'})
         if form == 'markdown':
             f = codecs.open(filename + '.md', 'w', encoding='utf-8')
             self.export2markdown(f, detail)
@@ -98,7 +100,7 @@ class Exporter(Analyzer):
 
     def run(self, link, f, form):
         self.export(link, f, form)
-        
+
 
 class Parser(Analyzer):
     """docstring for parser"""
@@ -109,22 +111,27 @@ class Parser(Analyzer):
 
     # get the articles' link
     def parse(self, html_doc):
-        soup = BeautifulSoup(html_doc)
-        res = self.getContent(soup).find(class_='list_item_new').find(id='article_list').find_all(class_='article_item')
+        soup = BeautifulSoup(html_doc,'lxml')
+        res = soup.findAll('div',{'class': 'article-item-box csdn-tracking-statistics'})
         i = 0
         for ele in res:
-            self.article_list.append('http://blog.csdn.net/' + ele.find(class_='article_title').h1.span.a['href'])
+            if i == 0 :
+                i = 1
+                continue
+            self.article_list.append(ele.find('p',{'class': 'content'}).a['href'])
 
     # get the page of the blog
     # may have a bug, because of the encoding
     def getPageNum(self, html_doc):
-        soup = BeautifulSoup(html_doc)
+        soup = BeautifulSoup(html_doc,'lxml')
+        print(soup.find('div',{'class': 'ui-paging-container'}))
         self.page = 1
         # papelist if a typo written by csdn front-end programmers?
-        pageList = self.getContent(soup).find(id='papelist')
+        pageList = soup.find(id='pageBox').findAll(attrs={'class': 'ui-pager'});
+        print(pageList)
         # if there is only a little posts in one blog, the papelist element doesn't even exist
         if pageList == None:
-        	print "Page is 1"
+        	print("Page is 1")
         	return 1
         res = pageList.span
         # get the page from text
@@ -139,9 +146,11 @@ class Parser(Analyzer):
 
     # get all the link
     def getAllArticleLink(self, url):
+        # print(url)
     	# get the num of the page
-        self.getPageNum(self.get(url))
+        # self.getPageNum(self.get(url))
         # iterate all the pages
+        self.page = 48;
         for i in range(1, self.page + 1):
             PrintLayer.printWorkingPage(i)
             self.parse(self.get(url + '/article/list/' + str(i)))
@@ -152,7 +161,7 @@ class Parser(Analyzer):
         for link in self.article_list:
             PrintLayer.printWorkingArticle(link)
             exporter = Exporter()
-            exporter.run(link, link.split('/')[7], form)
+            exporter.run(link, link.split('/')[6], form)
 
     # the page given
     def run(self, url, page=-1, form='markdown'):
@@ -165,12 +174,12 @@ class Parser(Analyzer):
             if page <= self.getPageNum(self.get(url)):
                 self.parse(self.get(url + '/article/list/' + str(page)))
             else:
-                print 'page overflow:-/'
+                print('page overflow:-/')
                 sys.exit(2)
         PrintLayer.printWorkingPhase('export')
         self.export(form)
         PrintLayer.printOver()
-    
+
 
 def main(argv):
     page = -1
@@ -179,13 +188,13 @@ def main(argv):
     form = 'markdown'
     try:
         opts, args = getopt.getopt(argv,"hu:f:p:o:")
-    except Exception, e:
-        print 'main.py -u <username> [-f <format>] [-p <page>] [-o <outputDirectory>]'
+    except Exception as e:
+        print('main.py -u <username> [-f <format>] [-p <page>] [-o <outputDirectory>]')
         sys.exit(2)
 
     for opt, arg in opts:
         if opt == '-h':
-            print 'main.py -u <username> [-f <format>] [-p <page>] [-o <outputDirectory>]'
+            print('main.py -u <username> [-f <format>] [-p <page>] [-o <outputDirectory>]')
             sys.exit()
         elif opt == '-u':
             username = arg
@@ -197,10 +206,10 @@ def main(argv):
             form = arg
 
     if username == 'default':
-        print 'Err: Username err'
+        print('Err: Username err')
         sys.exit(2)
     if form != 'markdown' and form != 'html':
-        print 'Err: format err'
+        print('Err: format err')
         sys.exit(2)
     url = 'http://blog.csdn.net/' + username
     parser = Parser()
